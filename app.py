c3fcb269b7c5b4d816dd89dc3b0b61deb2c8ad82
HEUKWU <yoo4068@gmail.com>
2023-01-16 14:09:07 +0900

    회원가입 페이지 CSS파일 수정

--- app.py
+++ app.py
@@ -31,14 +31,6 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg = "로그인 필요"))
 
-    # if request.cookies.get('mytoken') is not None:
-    #     token_receive = request.cookies.get('mytoken')
-    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #     user_info = db.user.find_one({"username": payload["id"]})
-    #     return render_template('index.html', user_info=user_info)
-    # else:
-    #     return render_template('login.html')
-
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
